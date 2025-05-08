3efde51c72091de6d944443160f4e86e0217b2c1
Victor Gabriel <victorgabrielcc22@gmail.com>
2025-05-08 19:29:06 -0300

    criacao sessao entidade

--- backend/src/main/java/com/hu/backend/entities/Sessao.java
+++ backend/src/main/java/com/hu/backend/entities/Sessao.java
@@ -1,12 +1,7 @@
 package com.hu.backend.entities;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
-import com.hu.backend.entities.converters.DurationConverter;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,11 +9,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -37,9 +36,7 @@ public class Sessao {
 
     private String observacoes;
 
-    @Column(name = "tempo_exposicao")
-    @Convert(converter = DurationConverter.class) 
-    private Duration tempoExposicao;
+    private Integer tempoExposicao;
 
     @ManyToOne
     @JoinColumn(name = "paciente_id", nullable = false) 
