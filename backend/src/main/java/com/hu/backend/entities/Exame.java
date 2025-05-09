e5e0d8a8f1d35bfc91e8db3fc13e114774fe37c1
Victor Gabriel <victorgabrielcc22@gmail.com>
2025-05-09 16:28:38 -0300

    imports

--- backend/src/main/java/com/hu/backend/entities/Exame.java
+++ backend/src/main/java/com/hu/backend/entities/Exame.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
