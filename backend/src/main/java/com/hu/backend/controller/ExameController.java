650b18eae77f7af5c6a946182c43a4fef5e90c40
Victor Gabriel <victorgabrielcc22@gmail.com>
2025-05-13 17:16:18 -0300

    feat: ExameController e ajustes

--- backend/src/main/java/com/hu/backend/controller/ExameController.java
+++ backend/src/main/java/com/hu/backend/controller/ExameController.java
@@ -1,9 +1,23 @@
 package com.hu.backend.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hu.backend.dto.exame.ExameCreationDto;
+import com.hu.backend.dto.exame.ExameDto;
 import com.hu.backend.service.ExameService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 
 @RestController
 @RequestMapping
@@ -16,10 +30,32 @@ public class ExameController {
     }
 
     //======================== GET =========================
-    
-    //======================== POST ========================
+        @GetMapping(value = "/exame")
+        public ResponseEntity<List<ExameDto>> findAll(){
+            return ResponseEntity.status(HttpStatus.OK).body(exameService.findAll());
+        }
 
+        @GetMapping(value = "/exame/{exameId}")
+        public ResponseEntity<ExameDto> findById(@PathVariable Long exameId){
+            return ResponseEntity.status(HttpStatus.OK).body(exameService.findById(exameId));
+        }
+
+        @GetMapping(value = "/tratamento/{tratamentoId}/exames")
+        public ResponseEntity<List<ExameDto>> findByTratamentoId(@PathVariable Long tratamentoId){
+            return ResponseEntity.status(HttpStatus.OK).body(exameService.findByTratamentoId(tratamentoId));
+        }
+    //======================== POST ========================
+        @PostMapping(value = "/exame")
+        public ResponseEntity<ExameDto> create(@RequestBody ExameCreationDto exameCreationDto){
+            return ResponseEntity.status(HttpStatus.CREATED).body(exameService.create(exameCreationDto));
+        }
+        
     //======================== PUT =========================
 
     //======================= DELETE =======================
+        @DeleteMapping(value = "/exame/{exameId}")
+        public ResponseEntity<Void> deleteById(@PathVariable Long exameId){
+            exameService.deleteById(exameId);
+            return ResponseEntity.noContent().build();
+        }
 }
