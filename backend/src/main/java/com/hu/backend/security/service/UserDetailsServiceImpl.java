5c7eb605ec67a460c195acdaf46224c6f8a71af1
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-05-23 11:04:23 -0300

    refatoração auth controller e criação de authservice

--- backend/src/main/java/com/hu/backend/security/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/hu/backend/security/service/UserDetailsServiceImpl.java
@@ -1,35 +1,24 @@
 package com.hu.backend.security.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import com.hu.backend.entities.Usuario;
 import com.hu.backend.repositories.UsuarioRepository;
+import com.hu.backend.security.model.UsuarioDetails;
 import com.hu.backend.service.exception.UserNotFound;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService{
-    
+public class UserDetailsServiceImpl implements UserDetailsService {
+
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(UserNotFound::new);
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(UserNotFound::new);
 
-        return new User(
-            usuario.getEmail(),
-            usuario.getPassword(),
-            List.of(new SimpleGrantedAuthority(usuario.getRole()))
-        );
+        return new UsuarioDetails(usuario);
     }
-
-
 }
