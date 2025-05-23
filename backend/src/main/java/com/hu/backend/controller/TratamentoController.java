52117ed15348ee86f778ece6d96b0628848a38bd
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2025-05-23 08:03:17 -0300

    feat: implement tratamento creation with exames and particularidade

---
+++ backend/src/main/java/com/hu/backend/controller/TratamentoController.java
@@ -0,0 +1,51 @@
+package com.hu.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hu.backend.dto.areaCorporalAcometida.AreaCorporalAcometidaCreationDto;
+import com.hu.backend.dto.particularidade.ParticularidadeCreationDto;
+import com.hu.backend.dto.particularidade.ParticularidadeDto;
+import com.hu.backend.dto.tratamento.TratamentoCreationDto;
+import com.hu.backend.dto.tratamento.TratamentoCreationRequest;
+import com.hu.backend.dto.tratamento.TratamentoDto;
+import com.hu.backend.service.TratamentoService;
+
+@RestController
+@RequestMapping
+public class TratamentoController {
+    
+    TratamentoService tratamentoService;
+
+    public TratamentoController(
+        TratamentoService tratamentoService
+    ) {
+        this.tratamentoService = tratamentoService;
+    }
+
+    //======================== GET =========================
+
+    //======================== POST ========================
+
+    @PostMapping(value = "/tratamento")
+    public ResponseEntity<TratamentoDto> create(
+        @RequestBody TratamentoCreationRequest tratamento
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tratamentoService.create(
+            tratamento.tratamento(),
+            tratamento.particularidade(),
+            tratamento.exames(),
+            tratamento.areaCorporal()
+            ));
+    }
+
+    //======================== PUT =========================
+
+    //======================= DELETE =======================
+
+
+}
--- backend/src/main/java/com/hu/backend/service/TratamentoService.java
+++ backend/src/main/java/com/hu/backend/service/TratamentoService.java
@@ -1,13 +1,24 @@
 package com.hu.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.hu.backend.dto.DtoUtils;
+import com.hu.backend.dto.areaCorporalAcometida.AreaCorporalAcometidaCreationDto;
+import com.hu.backend.dto.exame.ExameCreationDto;
+import com.hu.backend.dto.particularidade.ParticularidadeCreationDto;
 import com.hu.backend.dto.tratamento.TratamentoCreationDto;
 import com.hu.backend.dto.tratamento.TratamentoDto;
+import com.hu.backend.entities.AreaCorporalAcometida;
+import com.hu.backend.entities.Exame;
 import com.hu.backend.entities.Paciente;
+import com.hu.backend.entities.Particularidade;
 import com.hu.backend.entities.Tratamento;
+import com.hu.backend.repositories.AreaCorporalRepository;
+import com.hu.backend.repositories.ExameRepository;
 import com.hu.backend.repositories.PacienteRepository;
+import com.hu.backend.repositories.ParticularidadeRepository;
 import com.hu.backend.repositories.TratamentoRepository;
 import com.hu.backend.service.exception.PacienteNotFound;
 
@@ -16,8 +27,17 @@ public class TratamentoService {
     
     private TratamentoRepository tratamentoRepository;
     private PacienteRepository pacienteRepository;
+    private ParticularidadeRepository particularidadeRepository;
+    private ExameRepository exameRepository;
+    private AreaCorporalRepository areaCorporalRepository;
 
-    public TratamentoService(TratamentoRepository tratamentoRepository, PacienteRepository pacienteRepository){
+    public TratamentoService(
+        TratamentoRepository tratamentoRepository,
+        PacienteRepository pacienteRepository,
+        ParticularidadeRepository particularidadeRepository,
+        ExameRepository exameRepository,
+        AreaCorporalRepository areaCorporalRepository
+        ){
         this.tratamentoRepository = tratamentoRepository;
         this.pacienteRepository = pacienteRepository;
     }
@@ -25,17 +45,39 @@ public class TratamentoService {
     //======================== GET =========================
 
     //======================== POST ========================
-        public TratamentoDto create(TratamentoCreationDto tratamentoCreationDto){
+        public TratamentoDto create(
+            TratamentoCreationDto tratamentoCreationDto,
+            ParticularidadeCreationDto particularidadeCreationDto,
+            List<ExameCreationDto> exameListCreationDto,
+            AreaCorporalAcometidaCreationDto areaCorporalAcometidaCreationDto
+            ) {
             Paciente paciente = pacienteRepository.findById(tratamentoCreationDto.pacienteId()).orElseThrow(PacienteNotFound::new);
 
             Tratamento tratamento = DtoUtils.toEntity(tratamentoCreationDto);
-
             tratamento.setPaciente(paciente);
-            paciente.addTratamento(tratamento);
-            Tratamento tratamentoSaved = tratamentoRepository.save(tratamento);
-            pacienteRepository.save(paciente);
+
+            Particularidade particularidade = DtoUtils.toEntity(particularidadeCreationDto);
+            particularidade.setTratamento(tratamento);
+            tratamento.setParticularidade(particularidade);
+
+            AreaCorporalAcometida areaCorporal = DtoUtils.toEntity(areaCorporalAcometidaCreationDto);
+            areaCorporal.setTratamento(tratamento);
+            tratamento.setAreaCorporalAcometida(areaCorporal);
+
+            List<Exame> exames = exameListCreationDto.stream()
+                .map(dto -> {
+                    Exame exame = DtoUtils.toEntity(dto);
+                    exame.setTratamento(tratamento);
+                    return exame;
+                }).toList();
             
-            return DtoUtils.toDto(tratamentoSaved);
+            tratamento.setExames(exames);
+
+            paciente.addTratamento(tratamento);
+
+            pacienteRepository.save(paciente);
+
+            return DtoUtils.toDto(tratamento);
         }
     //======================== PUT =========================
 
